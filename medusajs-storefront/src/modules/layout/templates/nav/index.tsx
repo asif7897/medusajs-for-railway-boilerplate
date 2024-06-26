a642cca9259df93874c67c36a1267d9109625dc3
asif <123207572+asif7897@users.noreply.github.com>
2024-06-26 13:00:45 +0600

    Update index.tsx

--- medusajs-storefront/src/modules/layout/templates/nav/index.tsx
+++ medusajs-storefront/src/modules/layout/templates/nav/index.tsx
@@ -20,12 +20,15 @@ export default async function Nav() {
           </div>
 
           <div className="flex items-center h-full">
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-            >
-              Medusa Store
-            </LocalizedClientLink>
+
+            
+            
+           <img src="https://res.cloudinary.com/dqgrlf8uf/image/upload/v1719384891/rrrrrrrrrrrrrrrrrrrrrrrr_uzl74w.png" alt="sea sky">
+           </img>
+
+
+
+            
           </div>
 
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
