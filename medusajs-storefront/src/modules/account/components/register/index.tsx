f441b2109917337ffc73e9853989e99c3d821f2e
asif <123207572+asif7897@users.noreply.github.com>
2024-06-28 12:13:22 +0600

    Update index.tsx

--- medusajs-storefront/src/modules/account/components/register/index.tsx
+++ medusajs-storefront/src/modules/account/components/register/index.tsx
@@ -22,7 +22,7 @@ const Register = ({ setCurrentView }: Props) => {
         Become a Medusa Store Member
       </h1>
       <p className="text-center text-base-regular text-ui-fg-base mb-4">
-        Create your Medusa Store Member profile, and get access to an enhanced
+        Create your  SEA SKY Member profile, and get access to an enhanced
         shopping experience.
       </p>
       <form className="w-full flex flex-col" action={formAction}>
