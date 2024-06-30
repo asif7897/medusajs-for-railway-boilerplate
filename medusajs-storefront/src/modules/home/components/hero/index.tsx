b262d7ebce89701def433c520b7fdcfd99e04303
asif <123207572+asif7897@users.noreply.github.com>
2024-06-30 18:04:18 +0600

    Update index.tsx
    'use client';  // This directive ensures the component is rendered on the client side
    
     
    import 'react-alice-carousel/lib/alice-carousel.css';
    import "react-responsive-carousel/lib/styles/carousel.min.css";
    import { Carousel } from 'react-responsive-carousel';
    
     
    
    //const items = [
     // <img src="https://user-images.githubusercontent.com/63467479/195140534-b89c7ef6-916b-4c08-aef8-3cc51ee65284.png" onDragStart={handleDragStart} role="presentation" />,
     // <img src="https://user-images.githubusercontent.com/63467479/195140534-b89c7ef6-916b-4c08-aef8-3cc51ee65284.png" onDragStart={handleDragStart} role="presentation" />,
     // <img src="https://user-images.githubusercontent.com/63467479/195140534-b89c7ef6-916b-4c08-aef8-3cc51ee65284.png" onDragStart={handleDragStart} role="presentation" />,
    // ];
    
    const Hero = () => {
      return (
        <div className="h-[4vh] w-full relative">
          
          <div>
            <Carousel autoPlay interval={2500} infiniteLoop showThumbs={false} swipeable>
              <div>
                <img src="https://user-images.githubusercontent.com/72182438/195152867-721ca98b-bc8e-474f-8085-c6593382bc30.png" />
              </div>
              <div>
                <img src="https://user-images.githubusercontent.com/72182438/195152892-33d91384-a7fb-4c8a-bf17-aafc9017a49a.png" />
              </div>
              <div>
                <img src="https://user-images.githubusercontent.com/72182438/195152903-425d6a91-e7c7-4b51-82a4-377df8edaa82.png" />
              </div>
            </Carousel>
          </div>
          
        </div>
      );
    }
    
    export default Hero;

--- medusajs-storefront/src/modules/home/components/hero/index.tsx
+++ medusajs-storefront/src/modules/home/components/hero/index.tsx
@@ -1,24 +1,38 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
+'use client';  // This directive ensures the component is rendered on the client side
+
+ 
+import 'react-alice-carousel/lib/alice-carousel.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+ 
+
+//const items = [
+ // <img src="https://user-images.githubusercontent.com/63467479/195140534-b89c7ef6-916b-4c08-aef8-3cc51ee65284.png" onDragStart={handleDragStart} role="presentation" />,
+ // <img src="https://user-images.githubusercontent.com/63467479/195140534-b89c7ef6-916b-4c08-aef8-3cc51ee65284.png" onDragStart={handleDragStart} role="presentation" />,
+ // <img src="https://user-images.githubusercontent.com/63467479/195140534-b89c7ef6-916b-4c08-aef8-3cc51ee65284.png" onDragStart={handleDragStart} role="presentation" />,
+// ];
 
 const Hero = () => {
   return (
-    <div className="relative top-0 h-[90dvh] w-full border-b border-ui-border-base bg-ui-bg-subtle main-hero-section"
-    >
-      <div className="absolute left-12 md:left-52 inset-y-0 z-10 flex flex-col justify-center items-start text-left small:p-32 gap-6">
-        <h2 className="text-4xl">
-           S E A  S K Y 
-        </h2>
-        <a
-          target="_blank"
-        >
-          <Button variant="primary">
-            SHOP NOW
-          </Button>
-        </a>
+    <div className="h-[4vh] w-full relative">
+      
+      <div>
+        <Carousel autoPlay interval={2500} infiniteLoop showThumbs={false} swipeable>
+          <div>
+            <img src="https://user-images.githubusercontent.com/72182438/195152867-721ca98b-bc8e-474f-8085-c6593382bc30.png" />
+          </div>
+          <div>
+            <img src="https://user-images.githubusercontent.com/72182438/195152892-33d91384-a7fb-4c8a-bf17-aafc9017a49a.png" />
+          </div>
+          <div>
+            <img src="https://user-images.githubusercontent.com/72182438/195152903-425d6a91-e7c7-4b51-82a4-377df8edaa82.png" />
+          </div>
+        </Carousel>
       </div>
+      
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
