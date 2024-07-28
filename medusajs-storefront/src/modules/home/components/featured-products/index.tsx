3f0f5ea6a04bc1068ef14d22dd27b1b9a5010957
asif7897 <asifmahmud7894@gmail.com>
2024-07-29 04:02:34 +0600

    Describe your changes here

--- medusajs-storefront/src/modules/home/components/featured-products/index.tsx
+++ medusajs-storefront/src/modules/home/components/featured-products/index.tsx
@@ -1,18 +1,34 @@
-import { Region } from "@medusajs/medusa"
+import '../featured-products/product-rail/style.css'; // Path to your CSS file
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 
-import ProductRail from "@modules/home/components/featured-products/product-rail"
-import { ProductCollectionWithPreviews } from "types/global"
 
-export default async function FeaturedProducts({
-  collections,
-  region,
-}: {
-  collections: ProductCollectionWithPreviews[]
-  region: Region
-}) {
-  return collections.map((collection) => (
-    <li key={collection.id}>
-     
-    </li>
-  ))
+const FeaturedProducts: React.FC = () => {
+  const backgroundImageSrc = 'https://res.cloudinary.com/dqgrlf8uf/image/upload/v1719258169/samples/ecommerce/analog-classic.jpg';
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImageSrc})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1 style={{ fontSize: 50, color: "white", marginBottom: "20px" }}>Women Premium Suit</h1>
+      <div className="buttonLinkWrapper">
+        <LocalizedClientLink href="/collections/suit_w">
+          <span style={{ fontSize: 50, color: "white", marginBottom: "20px" }}>shop now </span>
+        </LocalizedClientLink>
+      </div>
+    </div>
+  );
 }
+
+export default FeaturedProducts;
+
