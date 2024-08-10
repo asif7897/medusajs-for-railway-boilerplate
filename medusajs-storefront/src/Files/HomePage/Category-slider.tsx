2f09405b1829fe3d78720291d275d314e569d669
asif7897 <asifmahmud7894@gmail.com>
2024-08-11 02:00:22 +0600

    category slider

--- medusajs-storefront/src/Files/HomePage/Category-slider.tsx
+++ medusajs-storefront/src/Files/HomePage/Category-slider.tsx
@@ -29,32 +29,32 @@ const CategorySlick: React.FC<CategorySlickProps> = () => {
     const slides: Slide[] = [
         {
             image: "https://res.cloudinary.com/dqgrlf8uf/image/upload/v1720379189/PKC_7433copy_grcqfn.jpg",
-            name: "Premium Suit",
+            name: "PREMIUM SUIT",
             value: "premium_suit"
         },
         {
             image: "https://res.cloudinary.com/dqgrlf8uf/image/upload/v1720379758/st1366_tiwmqi.jpg",
-            name: "Platinum Blazer",
+            name: "PLATINUM BLAZER",
             value: "platinum_blazer"
         },
         {
             image: "https://res.cloudinary.com/dqgrlf8uf/image/upload/v1720379503/PKC_6783copy_jfer1o.jpg",
-            name: "Elite Panjabi",
+            name: "ELITE PANJABI",
             value: "elite_panjabi"
         },
         {
             image: "https://res.cloudinary.com/dqgrlf8uf/image/upload/v1720379885/09-05-2024product00376_1024x1024_2x_j7tk27.jpg",
-            name: "Formal Shirt",
+            name: "FORMAL SHIRT",
             value: "formal_shirt"
         },
         {
             image: "https://res.cloudinary.com/dqgrlf8uf/image/upload/v1720380349/90_1010982-1A07978_1B000_10_WoolFormalPants-Clothing-Versace-online-store_1_3_x7zrpk.png",
-            name: "Formal Pant",
+            name: "FORMAL PANT",
             value: "formal_pant"
         },
         {
             image: "https://res.cloudinary.com/dqgrlf8uf/image/upload/v1720380496/A23WS0152363408_BLUE_vmxomo.jpg",
-            name: "Half Coat",
+            name: "HALF COAT",
             value: "half_coat"
         },
     ];
