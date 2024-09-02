08eb41bbbe40b21c8f403728d7df6e5ac0578831
asif7897 <asifmahmud7894@gmail.com>
2024-09-02 10:19:24 +0600

    women section

--- medusajs-storefront/src/modules/home/components/featured-products/index.tsx
+++ medusajs-storefront/src/modules/home/components/featured-products/index.tsx
@@ -1,38 +1,42 @@
+"use client"; // Add this line to indicate this is a Client Component
+
 import React from 'react';
 import '../featured-products/product-rail/style.css'; // Path to your CSS file
-import LocalizedClientLink from "@modules/common/components/localized-client-link";
 
 interface FeaturedProductsProps {
   collections: any; // Replace `any` with the actual type of collections
   region: any; // Replace `any` with the actual type of region
+  url?: string; // Optional URL for redirection (default: undefined)
 }
 
-const FeaturedProducts: React.FC<FeaturedProductsProps> = ({ collections, region }) => {
-  const backgroundImageSrc = 'https://res.cloudinary.com/dqgrlf8uf/image/upload/v1722540421/women_2_bkiqbm.webp';
+const FeaturedProducts: React.FC<FeaturedProductsProps> = ({
+  collections,
+  region,
+  url = '/collections/women\'s_suit', // Default URL set here
+}) => {
+  const frontImageSrc = 'https://res.cloudinary.com/dqgrlf8uf/image/upload/v1722540421/women_2_bkiqbm.webp';
+  const backImageSrc = 'https://res.cloudinary.com/dqgrlf8uf/image/upload/v1724674876/3-%287%29_1724674874305.png';
+
+  const handleClick = () => {
+    window.location.href = url; // Redirects to the specified URL
+  };
 
   return (
-    <div
-      className="featured-products-wrapper"
-      style={{
-        backgroundImage: `url(${backgroundImageSrc})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "flex-end",  // Aligns content to the bottom
-        justifyContent: "flex-end",  // Aligns content to the right
-        padding: "20px",  // Adds some padding from the edges
-      }}
-    >
-      <div className="buttonLinkWrapper">
-        <LocalizedClientLink href="/collections/women's_suit">
-          <span className="featured-products-link">Women wear</span>
-        </LocalizedClientLink>
+    <div className="featured-products-wrapper" onClick={handleClick}>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div
+            className="flip-card-front"
+            style={{ backgroundImage: `url(${frontImageSrc})` }}
+          />
+          <div
+            className="flip-card-back"
+            style={{ backgroundImage: `url(${backImageSrc})` }}
+          />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default FeaturedProducts;
