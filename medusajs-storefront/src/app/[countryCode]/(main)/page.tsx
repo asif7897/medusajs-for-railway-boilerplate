2a081375769ce5f7fd824e999880e29f20f45a11
asif7897 <asifmahmud7894@gmail.com>
2024-09-03 13:44:54 +0600

    accessory

--- medusajs-storefront/src/app/[countryCode]/(main)/page.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/page.tsx
@@ -117,20 +117,69 @@ export default async function Home({
       </div>
 
 
-     
-      <section className="pb-7 pt-10 space-y-1 px-4 md:px-8">
-  <h2 className="w-full text-center text-2xl lg:text-3xl font-semibold">
-    ACCESSORIES
-  </h2>
-  <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between items-start h-[95vh] md:h-[80vh]">
-    <a href="/collections/luxury_belt" className="w-full md:w-[calc(50%-8px)] h-full category-card-1 block">
-      {/* Content for Luxury Belt */}
+
+
+
+      <section className="pb-4 pt-6 space-y-1 px-4 md:px-8">
+      <h2 className="w-full text-center text-2xl lg:text-3xl font-semibold mb-4">
+  ACCESSORIES
+</h2>
+
+  <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row justify-between items-start h-[80vh] md:h-[70vh]">
+    
+    <a
+      href="/collections/luxury_belt"
+      className="w-full md:w-[calc(50%-8px)] h-full relative flex items-center justify-center overflow-hidden rounded-lg shadow-lg bg-gray-700 group"
+      style={{ backgroundImage: 'url("https://res.cloudinary.com/dqgrlf8uf/image/upload/v1724359769/Belt_Banner_e49irb.webp")' }}
+    >
+      <img
+        className="absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out"
+        src="https://res.cloudinary.com/dqgrlf8uf/image/upload/v1724359769/Belt_Banner_e49irb.webp"
+        alt="Luxury Belt"
+      />
+      <img
+        className="absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out opacity-0 group-hover:opacity-100"
+        src="https://res.cloudinary.com/dqgrlf8uf/image/upload/v1723909870/eghjh_1723909870068.png"
+        alt="Luxury Belt Hover"
+      />
+      <svg className="absolute inset-x-0 bottom-5 mx-auto text-3xl text-white transition-opacity duration-300 opacity-0 group-hover:opacity-100" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M2 10a4 4 0 0 1 4-4h20a4 4 0 0 1 4 4v10a4 4 0 0 1-2.328 3.635a2.996 2.996 0 0 0-.55-.756l-8-8A3 3 0 0 0 14 17v7H6a4 4 0 0 1-4-4V10Zm14 19a1 1 0 0 0 1.8.6l2.7-3.6H25a1 1 0 0 0 .707-1.707l-8-8A1 1 0 0 0 16 17v12Z" /></svg>
+      
+      <button className="absolute inset-0 flex items-center justify-center w-full h-full bg-black bg-opacity-50 text-gray-300 text-4xl font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+  Shop Now
+</button>
+
+
+
+      
     </a>
-    <a href="/collections/box_tie" className="w-full md:w-[calc(50%-8px)] h-full category-card-2 block">
-      {/* Content for Box Tie */}
+
+    <a
+      href="/collections/box_tie"
+      className="w-full md:w-[calc(50%-8px)] h-full relative flex items-center justify-center overflow-hidden rounded-lg shadow-lg bg-gray-700 group"
+      style={{ backgroundImage: 'url("https://res.cloudinary.com/dqgrlf8uf/image/upload/v1724359771/tie_section_mgdjoo.webp")' }}
+    >
+      <img
+        className="absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out"
+        src="https://res.cloudinary.com/dqgrlf8uf/image/upload/v1724359771/tie_section_mgdjoo.webp"
+        alt="Box Tie"
+      />
+      <img
+        className="absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out opacity-0 group-hover:opacity-100"
+        src="https://res.cloudinary.com/dqgrlf8uf/image/upload/v1723126267/RC-TIE-2.2_1723126266942.jpg"
+        alt="Box Tie Hover"
+      />
+      <svg className="absolute inset-x-0 bottom-5 mx-auto text-3xl text-white transition-opacity duration-300 opacity-0 group-hover:opacity-100" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M2 10a4 4 0 0 1 4-4h20a4 4 0 0 1 4 4v10a4 4 0 0 1-2.328 3.635a2.996 2.996 0 0 0-.55-.756l-8-8A3 3 0 0 0 14 17v7H6a4 4 0 0 1-4-4V10Zm14 19a1 1 0 0 0 1.8.6l2.7-3.6H25a1 1 0 0 0 .707-1.707l-8-8A1 1 0 0 0 16 17v12Z" /></svg>
+      <button className="absolute inset-0 flex items-center justify-center w-full h-full bg-black bg-opacity-50 text-gray-300 text-4xl font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+  Shop Now
+</button>
+
     </a>
   </div>
 </section>
+
+
+
+
 
 
 
