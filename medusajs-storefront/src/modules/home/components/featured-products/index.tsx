4955580914e90eb931aa201df12df5450b5ff54a
asif7897 <asifmahmud7894@gmail.com>
2024-09-04 10:48:15 +0600

    women url

--- medusajs-storefront/src/modules/home/components/featured-products/index.tsx
+++ medusajs-storefront/src/modules/home/components/featured-products/index.tsx
@@ -1,6 +1,7 @@
 "use client"; // Indicate this is a Client Component
 
 import React from 'react';
+import Link from 'next/link';
 import '../featured-products/product-rail/style.css'; // Ensure this path is correct
 
 interface FeaturedProductsProps {
@@ -12,26 +13,29 @@ const FeaturedProducts: React.FC<FeaturedProductsProps> = ({ collections, region
   const backgroundImageSrc = 'https://res.cloudinary.com/dqgrlf8uf/image/upload/v1722540421/women_2_bkiqbm.webp';
 
   return (
-    <div
-      className="featured-products-wrapper"
-      style={{
-        backgroundImage: `url(${backgroundImageSrc})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",  // Center content vertically
-        justifyContent: "center",  // Center content horizontally
-        padding: "20px",  // Adds some padding from the edges
-        position: "relative", // Ensures text overlay is positioned correctly
-      }}
-    >
-      <div className="text-overlay">
-        <span className="featured-products-text">Women Wear</span>
+    <Link href="/collections/women's_suit">
+      <div
+        className="featured-products-wrapper"
+        style={{
+          backgroundImage: `url(${backgroundImageSrc})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",  // Center content vertically
+          justifyContent: "center",  // Center content horizontally
+          padding: "20px",  // Adds some padding from the edges
+          position: "relative", // Ensures text overlay is positioned correctly
+          cursor: "pointer", // Change cursor to pointer to indicate it's clickable
+        }}
+      >
+        <div className="text-overlay">
+          <span className="featured-products-text">Women Wear</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
