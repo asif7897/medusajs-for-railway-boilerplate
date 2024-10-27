d99d4e70d32cc0d17784e3a37df2c087ff105f29
asif7897 <asifmahmud7894@gmail.com>
2024-10-28 02:31:35 +0600

    cart page.tsx for facebook event management

--- medusajs-storefront/src/app/[countryCode]/(main)/cart/page.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/cart/page.tsx
@@ -1,9 +1,7 @@
 import { LineItem } from "@medusajs/medusa"
 import { Metadata } from "next"
 import { cookies } from "next/headers"
-
 import CartTemplate from "@modules/cart/templates"
-
 import { enrichLineItems } from "@modules/cart/actions"
 import { getCheckoutStep } from "@lib/util/get-checkout-step"
 import { CartWithCheckoutStep } from "types/global"
@@ -14,6 +12,7 @@ export const metadata: Metadata = {
   description: "View your cart",
 }
 
+// Function to fetch cart details
 const fetchCart = async () => {
   const cartId = cookies().get("_medusa_cart_id")?.value
 
@@ -39,9 +38,42 @@ const fetchCart = async () => {
   return cart
 }
 
+// Main Cart component
 export default async function Cart() {
   const cart = await fetchCart()
   const customer = await getCustomer()
 
-  return <CartTemplate cart={cart} customer={customer} />
+  return (
+    <>
+      {/* Facebook Pixel Integration */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+            fbq('track', 'CardView');  // Custom event for viewing the cart
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=CardView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+
+      {/* Render the Cart Template */}
+      <CartTemplate cart={cart} customer={customer} />
+    </>
+  )
 }
